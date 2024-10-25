b2aa6496649bb7586761ed08929031ff386fc5a0
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-10-25 14:01:36 +0200

    Added more parameters

--- Assets/Fish.cs
+++ Assets/Fish.cs
@@ -8,10 +8,11 @@ public class Fish : MonoBehaviour
     public Genome<float> genome = new();
     private float size = 1f;
     private float colorHue = 0f;
+    private float speed = 1f;
+    public float bowlRadius = 10f;
 
     public float angleAlpha = 0;
     public float angleBeta = 0;
-    public float bowlRadius = 10f;
 
     public MeshRenderer fishRenderer;
 
@@ -20,28 +21,31 @@ public class Fish : MonoBehaviour
         var time = Time.time;
 
         transform.position = new Vector3(
-            bowlRadius * Mathf.Cos(angleAlpha + time) * Mathf.Cos(angleBeta),
+            bowlRadius * Mathf.Cos(angleAlpha + time * speed) * Mathf.Cos(angleBeta),
             bowlRadius * Mathf.Sin(angleBeta),
-            bowlRadius * Mathf.Sin(angleAlpha + time) * Mathf.Cos(angleBeta)
+            bowlRadius * Mathf.Sin(angleAlpha + time * speed) * Mathf.Cos(angleBeta)
         );
         transform.LookAt(new Vector3(
-            bowlRadius * Mathf.Cos(angleAlpha + time + 0.1f) * Mathf.Cos(angleBeta),
+            bowlRadius * Mathf.Cos(angleAlpha + (time + 0.1f) * speed) * Mathf.Cos(angleBeta),
             bowlRadius * Mathf.Sin(angleBeta),
-            bowlRadius * Mathf.Sin(angleAlpha + time + 0.1f) * Mathf.Cos(angleBeta)
+            bowlRadius * Mathf.Sin(angleAlpha + (time + 0.1f) * speed) * Mathf.Cos(angleBeta)
         ));
     }
 
-    public void Place(float angleB, float angleA, float radius)
+    // public void Place(float angleB, float angleA, float radius)
+    public void Place(float angleB, float angleA)
     {
         angleBeta = angleB;
         angleAlpha = angleA;
-        bowlRadius = radius;
+        // bowlRadius = radius;
     }
 
     public void ApplyGenotype()
     {
         size = genome.genes[0];
         colorHue = genome.genes[1];
+        speed = genome.genes[2];
+        bowlRadius = genome.genes[3];
 
         ApplyPhenotype();
     }
--- Assets/FishManager.cs
+++ Assets/FishManager.cs
@@ -11,8 +11,10 @@ public class FishManager : MonoBehaviour
 
     public GameObject fishPrefab;
 
-    public float fitSize = 1f;
+    public float fitSize = .5f;
     public float fitColorHue = 0f;
+    public float fitSpeed = .5f;
+    public float fitBowlRadius = 5f;
 
     public int size = 50;
     public int elitism = 15;
@@ -27,6 +29,8 @@ public class FishManager : MonoBehaviour
         generations[currentGeneration].InitializeGeneRanges(new List<Tuple<float, float>> {
             new(.5f, 2f), // size
             new(0f, 360f), // colorHue
+            new(.5f, 2f), // speed
+            new(5f, 12f), // bowlRadius
         });
         generations[currentGeneration].InitializePopulation(size);
 
@@ -45,6 +49,14 @@ public class FishManager : MonoBehaviour
     public void SetFitColorHue(float value)
     {
         fitColorHue = value;
+    }
+    public void SetFitSpeed(float value)
+    {
+        fitSpeed = value;
+    }
+    public void SetFitBowlRadius(float value)
+    {
+        fitBowlRadius = value;
     }
 
     public int Next()
@@ -95,8 +107,8 @@ public class FishManager : MonoBehaviour
                 fish.ApplySelected(populationToShow.IsSelected(population[i]));
                 fish.Place(
                     Mathf.Lerp(Mathf.PI * -(.5f - .125f), Mathf.PI * (.5f - .125f), (float)i / population.Count),
-                    UnityEngine.Random.Range(0, 2 * Mathf.PI),
-                    UnityEngine.Random.Range(5f, 12f)
+                    UnityEngine.Random.Range(0, 2 * Mathf.PI)
+                // , UnityEngine.Random.Range(5f, 12f)
                 );
                 fishes.Add(fish);
                 fish.transform.parent = transform;
@@ -108,17 +120,23 @@ public class FishManager : MonoBehaviour
     {
         if (i < 0) i = (generations.Count + i) % generations.Count;
 
+        // We use the Lorentzian function to calculate the fitness of each gene
+        // And we multiply them to get the final fitness
         generations[i].EvaluatePopulation((Genome<float> genome) =>
         {
             float size = genome.genes[0];
             float colorHue = genome.genes[1] / 360f * 10f;
+            float speed = genome.genes[2];
+            float bowlRadius = genome.genes[3];
 
             float fitColorHue = this.fitColorHue / 360f * 10f;
 
             float sizeFitness = 1f / (1f + Mathf.Pow(size - fitSize, 2));
             float colorFitness = 1f / (1f + Mathf.Pow(Mathf.Min(Mathf.Abs(colorHue - fitColorHue), 10f - Mathf.Abs(colorHue - fitColorHue)), 2));
+            float speedFitness = 1f / (1f + Mathf.Pow(speed - fitSpeed, 2));
+            float radiusFitness = 1f / (1f + Mathf.Pow(bowlRadius - fitBowlRadius, 2));
 
-            return colorFitness * sizeFitness;
+            return colorFitness * sizeFitness * speedFitness * radiusFitness;
         });
     }
 
